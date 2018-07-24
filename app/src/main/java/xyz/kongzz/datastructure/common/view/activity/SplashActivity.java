5295d42d6a8da0f0791b9250d2b16795f896e733
DESKTOP-1N3HAER\Kong <love10522@foxmail.com>
2018-07-24 10:08:40 +0800

    增加启动页

---
+++ app/src/main/java/xyz/kongzz/datastructure/common/view/activity/SplashActivity.java
@@ -0,0 +1,17 @@
+package xyz.kongzz.datastructure.common.view.activity;
+
+import xyz.kongzz.datastructure.R;
+import xyz.kongzz.datastructure.common.base.BaseActivity;
+
+/**
+ * 作者:Kong
+ * 时间:2018/7/2316:35
+ * 描述:这是 SplashActivity
+ */
+
+public class SplashActivity extends BaseActivity {
+    @Override
+    protected int getContentLayoutId() {
+        return R.layout.activity_splash;
+    }
+}
