0bd49b5ac999039bf23ba4ed4edb93dac2a9b598
DESKTOP-1N3HAER\Kong <love10522@foxmail.com>
2018-07-25 16:43:32 +0800

    优化架构以及增加viewpager

---
+++ app/src/main/java/xyz/kongzz/datastructure/common/base/PresenterToolbarActivity.java
@@ -0,0 +1,99 @@
+package xyz.kongzz.datastructure.common.base;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+
+import xyz.kongzz.datastructure.R;
+import xyz.kongzz.datastructure.factory.presenter.BaseContract;
+
+/**
+ * 作者:Kong
+ * 时间:2018/7/2514:45
+ * 描述:这是 PresenterToolbarActivity
+ */
+
+public abstract class PresenterToolbarActivity<Presenter extends BaseContract.Presenter>
+        extends ToolbarActivity implements BaseContract.View<Presenter>{
+    protected Presenter mPresenter;
+    protected ProgressDialog mLoadingDialog;
+
+    @Override
+    protected void initBefore() {
+        super.initBefore();
+        // 初始化Presenter
+        initPresenter();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 界面关闭时进行销毁的操作
+        if (mPresenter != null) {
+            mPresenter.destroy();
+        }
+    }
+
+    /**
+     * 初始化Presenter
+     *
+     * @return Presenter
+     */
+    protected abstract Presenter initPresenter();
+
+    @Override
+    public void showError(int str) {
+        hideDialogLoading();
+        // 显示错误, 优先使用占位布局
+        if (mPlaceHolderView != null) {
+            mPlaceHolderView.triggerError(str);
+        } else {
+            DSApplication.showToast(str);
+        }
+    }
+
+    @Override
+    public void showLoading() {
+        if (mPlaceHolderView != null) {
+            mPlaceHolderView.triggerLoading();
+        } else {
+            ProgressDialog dialog = mLoadingDialog;
+            if (dialog == null) {
+                dialog = new ProgressDialog(this, R.style.AppTheme_Dialog_Alert_Light);
+                // 不可触摸取消
+                dialog.setCanceledOnTouchOutside(false);
+                // 强制取消关闭界面
+                dialog.setCancelable(true);
+                dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        finish();
+                    }
+                });
+                mLoadingDialog = dialog;
+            }
+            dialog.setMessage(getText(R.string.prompt_loading));
+            dialog.show();
+        }
+    }
+
+    protected void hideDialogLoading(){
+        ProgressDialog dialog = mLoadingDialog;
+        if (dialog != null) {
+            mLoadingDialog = null;
+            dialog.dismiss();
+        }
+    }
+
+    protected void hideLoading() {
+        hideDialogLoading();
+        if (mPlaceHolderView != null) {
+            mPlaceHolderView.triggerOk();
+        }
+    }
+
+    @Override
+    public void setPresenter(Presenter presenter) {
+        // View中赋值Presenter
+        mPresenter = presenter;
+    }
+}
